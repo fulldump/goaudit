7b5baf219a959d89366500b73158f79a106eb053
fulldump <gerardooscarjt@gmail.com>
2018-01-21 18:57:30 +0100

    Chan2Mongo: Handle if insertion is duplicated

--- Chan2Mongo.go
+++ Chan2Mongo.go
@@ -3,6 +3,8 @@ package goaudit
 import (
 	"time"
 
+	"fmt"
+
 	mgo "gopkg.in/mgo.v2"
 )
 
@@ -42,6 +44,9 @@ func Chan2Mongo(s chan *Audit, m *mgo.Database) {
 			c := m.C(name)
 
 			for e := c.Insert(audit); nil != e; e = c.Insert(audit) {
+				if mgo.IsDup(e) {
+					break
+				}
 				// TODO: alarm warning?
 				time.Sleep(1 * time.Second)
 			}
